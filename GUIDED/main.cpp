bb8095540c8ab1a014172890b70c5c2b863278fd
rifkitaufikurrohman <pakiwiki123@gmail.com>
2024-09-27 23:10:44 +0700

    upload task file

---
+++ GUIDED/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+// Code ke-1
+    // int main() {
+    //     cout << "Hello Ardhian" << endl;     
+    //     return 0; 
+    // }
+
+// Code ke-2
+    // int main() {     
+    //     int inp;     
+    //     cin >> inp;  
+    //     cout << "nilai = " << inp << endl;  
+    //     return 0;  
+    // }
+
+// Code ke-3
+    // int main() {
+    //     float W, X, Y; float Z;    
+    //     X = 7; Y = 3; W=1;    
+    //     Z = (X+Y) / (Y+W);    
+    //     cout << "nilai Z = " << Z << endl;  
+    //     return 0;   
+    // }
+
+// Code ke-4
+    // int main() {
+    //     double tot_beli, diskon;
+    //     cout << "Total beli = Rp ";
+    //     cin >> tot_beli;
+
+    //     diskon = 0;
+
+    //     if (tot_beli >= 100000)
+    //         diskon = 0.75 * tot_beli; 
+    //     else
+    //         diskon = 0;
+
+    //     cout << "Besar diskon = Rp" << diskon << endl;
+
+    //     return 0;  
+    // }
+
+// Code ke-5
+    // int main(){    
+    //     int kode_hari;    
+    //     puts("Tentuin hari\n");    
+    //     puts("");  
+    // }
+
+// Code ke - 6
+    #include <conio.h>  
+    int main(){    
+        int i=1;  
+        int jum;    
+        cin >>jum;  
+
+        do{  
+            cout << "Baris ke-" << i+1 << endl;       
+            i++;  
+        } while(i<jum);         
+            getch();  
+            return 0;  
+    }
+
---
+++ UNGUIDED/if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai;
+
+    cout << "Masukkan Nilai : ";
+    cin >> nilai;
+    if( nilai > 80 ) {
+        cout << "A" << endl;
+    } else {
+        cout << "Bukan A" << endl;
+    }
+
+    return 0;
+}
